cf1132cf8b342b53e29cd23613d330734ed1ddf9
Ahmed Dhaouadi <dapbarka@gmail.com>
2020-11-29 16:22:23 +0100

    'api_commit'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import "./App.css";
+import UserList  from "./UserList";
+import Menu from "./Menu";
+import React from 'react'
+import Home from './Home'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Menu />
+      <div className="App">
+        <Route exact path='/' component={Home}/>
+        <Route path="/UserList" component={UserList} />
+      </div>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+    return(
+        <div className="text">
+        <h2>Welcome to the City</h2>
+        <p>
+          NewYorkers is a revolutionary concept that create bonds between users
+          of the same city through a well moderated application
+        </p>
+        <h1>Under Construction</h1>
+      </div>
+    )
+}
+
+
+export default Home;
